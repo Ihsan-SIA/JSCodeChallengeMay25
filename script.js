2b1ae247d566c88c14f5e20d9b31b941942f8584
Ihsan-Ahmad Olamide Sodeko <ihsan.ahmadsodeko@gmail.com>
2025-05-19 16:36:59 +0100

    Series of code challenge in may of 2025

---
+++ script.js
@@ -0,0 +1,52 @@
+// const DivideTwoNums = (firstNo, secNo) => {
+//     const c = firstNo / secNo;
+//     alert(`Your answer is ${c}`);
+// }
+// firstNo = parseFloat(prompt("Enter first no"));
+// secNo = parseFloat(prompt("Enter sec no"));
+// DivideTwoNums(firstNo, secNo);
+
+
+// const GetEvenOrOdd = function (a) {
+//     if (a % 2 === 0) {
+//         alert(`${a} is an even number`)
+//     }
+//     else if (a % 2 !== 0){
+//         alert(`${a} is an odd number`)
+//     }
+//     else{
+//         alert("Sorry there's a wrong input somewhere");
+//     }
+// }
+// const a = parseInt(prompt("enter the number you want to check if odd or even"));
+// GetEvenOrOdd(a);
+
+// const LargestOfThree = function (a, b, c) {
+
+//     if (a > b & a > c) {
+//         return `${a} is the largest of the three`;
+//     }
+//     else if (b > a & b > c) {
+//         return `${b} is the largest of the three`;
+//     }
+//     else if (c > a & c > b) {
+//         return `${c} is the largest of the three`;
+//     }
+//     else {
+//         return "Sorry there's an incorrect input somewhere";
+//     }
+// }
+// a = parseInt(prompt("Enter the first number"));
+// b = parseInt(prompt("Enter the second number"));
+// c = parseInt(prompt("Enter the third number"));
+// alert(LargestOfThree(a, b, c));
+
+
+const ReverseString = function (aString){
+    let anArray = aString.split("")
+    let bArray = anArray.reverse()
+    aString = bArray.join("");
+    return aString;
+}
+aString = prompt("Enter your string here");
+alert(ReverseString(aString));
